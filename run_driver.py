5087d8f7f13d4048bb499cb13c2582730415d73c
gb10udn <rbbpx0926@gmail.com>
2024-01-23 23:02:22 +0900

    hoge

--- run_driver.py
+++ run_driver.py
@@ -7,3 +7,4 @@ if __name__ == '__main__':
     cmd = 'http://localhost:{}/session/{}/screenshot'.format(driver.service.port, driver.session_id)
     print(cmd)
     input('To exit, please enter the enter key')
+    
