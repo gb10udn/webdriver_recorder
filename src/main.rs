bf2e6af7d94570254406b390e9a98cb0bd8b8bd9
gb10udn <rbbpx0926@gmail.com>
2024-02-01 22:18:24 +0900

    session_id / port_number を別で取得できるようにした。

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://localhost:51581/session/a0201acbf42d389d612062214520e1f2/screenshot")  // INFO: 240201 パラメタ設定すると、base64 でとれた。
+    let port_number = "51581";
+    let session_id = "a0201acbf42d389d612062214520e1f2";
+    let url = format!("http://localhost:{}/session/{}/screenshot", port_number, session_id);
+    let resp = reqwest::get(url)  // INFO: 240201 パラメタ設定すると、base64 でとれた。
         .await?
         .json::<HashMap<String, String>>()
         .await?;
-    println!("{:#?}", resp);
+    println!("{:#?}", resp);  // EDIT: 240201 base64 を保存できるようにする。
     Ok(())
 }
