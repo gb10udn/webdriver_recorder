b1485e1ac89008214f1a805d05b48b0b562166d0
gb10udn <rbbpx0926@gmail.com>
2024-02-01 22:10:11 +0900

    base64 でスクリーンショットをとる機能を追加。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("http://localhost:51581/session/a0201acbf42d389d612062214520e1f2/screenshot")  // INFO: 240201 パラメタ設定すると、base64 でとれた。
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
