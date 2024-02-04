8b3a6924b73819d4589fd997c4137500e646c2cc
gb10udn <rbbpx0926@gmail.com>
2024-02-04 20:42:51 +0900

    複数画像を保存するようにした。

--- run_driver.py
+++ run_driver.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
-
+import pyperclip
 
 if __name__ == '__main__':
     driver = webdriver.Edge()
     driver.get('https://www.google.com/')
-    cmd = 'http://localhost:{}/session/{}/screenshot'.format(driver.service.port, driver.session_id)
-    print(cmd)
+
+    cmd = f'cargo run -- -p {driver.service.port} -s {driver.session_id}'
+    pyperclip.copy(cmd)
+    print(f'\n{cmd}\n')
+    
     input('To exit, please enter the enter key')
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 use std::fs::File;
+use std::fs;
 use std::io::Write;
 use base64::prelude::*;
 use clap::Parser;
-use std::time::Instant;
 
 
 /// webdriver の録画を実行する関数。
@@ -12,36 +12,36 @@ use std::time::Instant;
 /// Ex.2 .\target\debug\webdriver-recorder.exe -p 65478 -s f33e6812e7efb6e926b8801cf60f94e8 (.exe 実行時は、-- 不要)
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let start = Instant::now();
-    let is_debug_mode = true;  // FIXME: 240203 リリース時には false に戻すこと。
+    // [START] set params
+    let base_dst_dir = "./dst";
+    fs::create_dir_all(base_dst_dir)?;
+    // [END] set params
+
     let args = Args::parse();
     let url = format!("http://localhost:{}/session/{}/screenshot", args.port_number, args.session_id);
 
-    if is_debug_mode {print_duration(start)};
-
-    let resp = reqwest::get(url)  // INFO: 240201 パラメタ設定すると、base64 でとれた。
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-
-    if is_debug_mode {print_duration(start)};  // INFO: 240203 ここで全体の 95% 近くの時間がかかる。(= webdriver api の応答待ちが律速。)
     
-    if let Some(base64_string) = resp.get("value") {  // TODO: 240202 while true でループさせて、エラーが連続で３回とか出たら処理落とすとかにするといいかも？
-        let binary_data = BASE64_STANDARD.decode(base64_string).unwrap();
-        let file_path = "output.png";  // TODO: 240203 上書きするのではなくて、名前を微妙に変更するようにする。(開始からの時間をファイル名に入れてもいいかも？)
-        let mut file = File::create(file_path).expect("Unable to create file");
-        file.write_all(&binary_data).expect("Unable to write data to file");
-        print_duration(start);
+    let mut image_index = 0;
+    loop {
+        let resp = reqwest::get(&url)  // INFO: 240201 パラメタ設定すると、base64 でとれた。
+            .await?
+            .json::<HashMap<String, String>>()
+            .await?;
+
+        if let Some(base64_string) = resp.get("value") {
+            let binary_data = BASE64_STANDARD.decode(base64_string).unwrap();
+            let file_path = format!("{}/{}.png", base_dst_dir, image_index);
+            let mut file = File::create(file_path).expect("Unable to create file");
+            file.write_all(&binary_data).expect("Unable to write data to file");
+        }
+
+        image_index += 1;
+        if image_index > 100 {  // TODO: 240204 エラー３回で抜けるなどのように実装せよ。
+            break;
+        }
     }
 
-    if is_debug_mode {print_duration(start)};
-    
     Ok(())
-}
-
-fn print_duration(start: Instant) {
-    let end = start.elapsed();
-    println!("{}.{:03} second passed", end.as_secs(), end.subsec_nanos() / 1_000_000);
 }
 
 
@@ -49,7 +49,7 @@ fn print_duration(start: Instant) {
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// webdriver のポート番号
-    #[arg(short = 'p', long)]
+    #[arg(short = 'p', long)]  // INFO: 240204 short を設定すると、-p 54321 などのように呼び出せる。
     port_number: String,
 
     /// webdriver のセッション ID
